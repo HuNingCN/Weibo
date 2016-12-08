72bb949a3974dd90308acf7a6a3f7785e3d37e20
HuNingCN <huning_a@163.com>
2016-12-08 23:42:12 +0800

    初始化项目

---
+++ 微博/微博/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  微博
+//
+//  Created by 胡宁 on 2016/12/8.
+//  Copyright © 2016年 胡宁. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
